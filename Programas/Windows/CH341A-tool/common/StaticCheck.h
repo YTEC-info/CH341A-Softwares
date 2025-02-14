a1bbf46ef644559064ec3d9f5ccf089bd4937d8b
YTEC-info <73857225+YTEC-info@users.noreply.github.com>
2025-02-14 13:01:59 -0300

    Delete Programas/Windows directory
    See issue #7 #18

--- Programas/Windows/CH341A-tool/common/StaticCheck.h
+++
@@ -1,8 +0,0 @@
-#ifndef StaticCheckH
-#define StaticCheckH
-
-template<int> struct CompileTimeError;
-template<> struct CompileTimeError<true> {};
-#define STATIC_CHECK(expr, msg) { CompileTimeError<((expr) != 0)> ERROR_##msg; (void)ERROR_##msg; } 
-
-#endif
--- Programas/Windows/CH341A-tool/common/common.cpp
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-#if 0
-#include <vcl.h>
-#pragma hdrstop
-#endif
-#define Library
-
-// To add a file to the library use the Project menu 'Add to Project'.
-
